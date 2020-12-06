0055e41ec5d12044b227ab28787c83cb51a5a100
Gean Araujo <icrazy.gean@gmail.com>
2020-12-06 00:31:19 -0300

    jksd

--- public/scripts/cadastro.js
+++ public/scripts/cadastro.js
@@ -50,3 +50,16 @@ send.addEventListener('submit', (e) => {
         e.preventDefault()
     }
 })
+
+
+const gotop4 = document.querySelector('#gotop4')
+const third_land = document.querySelector('.third-land')
+const fourth_land = document.querySelector('.fourth-land')
+
+gotop4.addEventListener('click', (e) => {
+    e.preventDefault()
+    third_land.style.transform = "translateX(-100%)"
+    third_land.style.opacity = "0"
+    fourth_land.style.opacity = "1"
+    fourth_land.style.pointerEvents = "all"
+})
